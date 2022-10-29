253b6dc61fe6a3ddf77537908452a322951d4207
Antonis Messinis <ant_messinis@hotmail.gr>
2022-10-30 01:36:57 +0300

    Added Toastr notifications

---
+++ LibraryWebApp .NET-Blazor/AdminPanel/Helper/IJSRuntimeExtention.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace AdminPanel.Helper
+{
+    public static class IJSRuntimeExtention
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+
+        public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
---
+++ LibraryWebApp .NET-Blazor/AdminPanel/wwwroot/js/toastr.js
@@ -0,0 +1,9 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, 'Operation Success', { timeOut: 5000 })
+    }
+
+    if (type === "error") {
+        toastr.error(message, 'Operation fail', { timeOut: 5000 })
+    }
+}
